e5996a0efc2c5730204a00f1d72ccca70e0ef24b
Alex Brown <alexanderbrown.info@gmail.com>
2017-11-10 09:11:38 +0000

    paragraph populates on search submit

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,8 +1,8 @@
 <template>
-  <form>
+  <form v-on:submit="searchSubmit">
     <label>Search:</label>
     <input v-model='searchText'>
-    <p>{{ searchText }}</p>
+    <p>{{ searchResult }}</p>
     <button type="submit">Submit</button>
   </form>
 </template>
@@ -12,7 +12,14 @@
     name: 'searchInput',
     data: function() {
       return {
-        searchText: ''
+        searchText: '',
+        searchResult: ''
+      }
+    },
+    methods: {
+      searchSubmit: function(event) {
+        event.preventDefault();
+        this.searchResult = event.target[0].value;
       }
     }
   }
